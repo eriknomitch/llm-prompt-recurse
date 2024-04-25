6aa1eb084b9fa7ffdef4b944d41de264171a28c0
Erik Nomitch <enomitch@gmail.com>
2024-04-24 23:05:03 -0500

    Refactored interactive.py to prompt user for task, input variables, and output variables.

--- interactive.py
+++ interactive.py
@@ -1,26 +1,4 @@
 import click
-# Function to load JSON data from a file
-PROMPTS = {
-    'email_contractor': {
-        'input_variables': [
-            'contractor_name',
-            'project_details',
-            'discount_justification'
-        ]
-    },
-    'linkedin_from_tweet': {
-        'input_variables': ['tweet']
-    },
-    'markdown_resume_builder': {
-        'input_variables': ['sample_resume', 'candidate_information']
-    },
-    'job_fit': {
-        'input_variables': ['job_post', 'candidate_description']
-    },
-    'code_analysis': {
-        'input_variables': ['job_post', 'candidate_description']
-    }
-}
 
 # Function to prompt for input based on the keys in the JSON file
 def prompt_for_input(input_variables):
@@ -33,13 +11,10 @@ def prompt_for_input(input_variables):
 @click.command()
 @click.argument('prompt_filename')
 def main(prompt_filename):
-    # Retrieve the input variables from the PROMPTS dictionary
-    prompt_data = PROMPTS.get(prompt_filename)
-
-    # Extract the input variables from the JSON data
-    task = click.prompt("Please enter your task")
-    input_variables = click.prompt("Please enter your input variables (comma-separated)", value_proc=lambda x: x.split(','))
-    output_variables = click.prompt("Please enter your output variables (comma-separated)", value_proc=lambda x: x.split(','))
+    # Prompt the user for task, input variables, and output variables
+    task = click.prompt("Please enter the task")
+    input_variables = click.prompt("Please enter the input variables (comma-separated)", value_proc=lambda x: x.split(','))
+    output_variables = click.prompt("Please enter the output variables (comma-separated)", value_proc=lambda x: x.split(','))
 
     # Prompt the user for input and store the responses
     responses = prompt_for_input(input_variables)
